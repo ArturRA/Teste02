a66e6bb68d0f958e92606fae037be2d86da845fd
Artur Ribeiro Alfa <phoenixrebirth1324@gmail.com>
2023-03-13 02:44:44 -0300

    Requisito 1.3
    Como funcionário, Junior quer ter a possibilidade de editar um equipamento, sendo que ele possa editar todos os campos.

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ class Maquina:
 		self.fabricante = fabricante
 
 
-# Verifica se a maquina e valido
-def register_machine(empresa):
-
-	# 1. Validar nome da maquina
+def validar_nome_maquina():
 	nome_maquina = ''
 	while True:
 		nome_maquina = input('Digite o nome da maquina(deve possuir no minimo 6 digitos): ')
@@ -21,8 +18,9 @@ def register_machine(empresa):
 			continue
 		else:
 			break
+	return nome_maquina
 
-	# 2. Validar preco de aquisicao da maquina
+def validar_preco_de_aquisicao_da_maquina():
 	preco_de_aquisicao_da_maquina = 0
 	while True:
 		try:
@@ -34,8 +32,9 @@ def register_machine(empresa):
 			break
 		else:
 			print('Input deve ser maior que zero')
-	
-	# 3. Validar numero de serie
+	return preco_de_aquisicao_da_maquina
+
+def validar_numero_de_serie():
 	numero_de_serie = ''
 	while True:
 		numero_de_serie = input('Digite o numero de serie: ')
@@ -44,8 +43,9 @@ def register_machine(empresa):
 			continue
 		else:
 			break
+	return numero_de_serie
 
-	# 4. Valida data de fabricacao
+def validar_data_de_fabricacao():
 	data_de_fabricacao = ''
 	while True:
 		data_de_fabricacao = input('Digite a data de fabricacao no formato dd/mm/yyyy: ')
@@ -59,23 +59,92 @@ def register_machine(empresa):
 			break
 		else:
 			print("Input date is not valid..")
+	return data_de_fabricacao
 
-	# 5. Validar fabricante
+def validar_fabricante():
 	fabricante = ''
 	while True:
 		fabricante = input('Digite o nome da fabricante: ')
-		if len(numero_de_serie) < 1:
+		if len(fabricante) < 1:
 			print('Input e muito curto.')
 			continue
 		else:
 			break
+	return fabricante
+
+# 1.1 Registra maquina na empresa
+def register_machine(empresa):
+
+	# 1. Validar nome da maquina
+	nome_maquina = validar_nome_maquina()
+
+	# 2. Validar preco de aquisicao da maquina
+	preco_de_aquisicao_da_maquina = validar_preco_de_aquisicao_da_maquina()
+	
+	# 3. Validar numero de serie
+	numero_de_serie = validar_numero_de_serie()
+
+	# 4. Validar data de fabricacao
+	data_de_fabricacao = validar_data_de_fabricacao()
+
+	# 5. Validar fabricante
+	fabricante = validar_fabricante()
+
 	empresa.append(Maquina(nome_maquina, preco_de_aquisicao_da_maquina, numero_de_serie, data_de_fabricacao, fabricante))
 
-# Imprime uma lista de todas as maquinas registradas
+# 1.2 Imprime uma lista de todas as maquinas registradas
 def check_machines(empresa):
 	print('Lista de maquinas registradas:')
 	for x in range(len(empresa)):
-		print(str(x + 1) + ': Nome da maquina: ' + empresa[x].nome_maquina + '; numero de serie: ' + empresa[x].numero_de_serie + '; fabricante: ' + empresa[x].fabricante)
+		#print(str(x + 1) + ': Nome da maquina: ' + empresa[x].nome_maquina + '; numero de serie: ' + empresa[x].numero_de_serie + '; fabricante: ' + empresa[x].fabricante)
+		print(empresa[x].__dict__)
+
+# 1.3 Editar maquina registrada
+def edit_machines(empresa):
+	if len(empresa) == 0:
+		print('Empresa nao possui maquinas registradas.')
+	else:
+		check_machines(empresa)
+		maquina_para_editar = ''
+		while True:
+			maquina_para_editar = int(input('Digite o numero da maquina que deseja editar: ')) - 1
+			if 0 <= maquina_para_editar < len(empresa):
+				break
+			else:
+				print('Input nao esta dentro do tamanho da lista.')
+				continue
+	while True:
+		operacao_de_edicao  = input('Digite o numero da operacao desejada:\n' +
+					  '1. Editar nome da maquina.\n' +
+					  '2. Editar preco de aquisicao da maquina.\n' +
+					  '3. Editar numero de serie.\n' +
+					  '4. Editar data de fabricacao.\n' +
+					  '5. Editar fabricante.\n' +
+					  '6. Parar a edicao.\n')
+		match operacao_de_edicao:
+			case '1':
+				nome_maquina = validar_nome_maquina()
+				empresa[maquina_para_editar].nome_maquina = nome_maquina
+				continue
+			case '2':
+				preco_de_aquisicao_da_maquina = validar_preco_de_aquisicao_da_maquina()
+				empresa[maquina_para_editar].preco_de_aquisicao_da_maquina = preco_de_aquisicao_da_maquina
+				continue
+			case '3':
+				numero_de_serie = validar_numero_de_serie()
+				empresa[maquina_para_editar].numero_de_serie = numero_de_serie
+				continue
+			case '4':
+				data_de_fabricacao = validar_data_de_fabricacao()
+				empresa[maquina_para_editar].data_de_fabricacao = data_de_fabricacao
+				continue
+			case '5':
+				fabricante = validar_fabricante()
+				empresa[maquina_para_editar].fabricante = fabricante
+				continue
+			case _:
+				break
+
 
 if __name__ == "__main__":
 
@@ -84,16 +153,20 @@ if __name__ == "__main__":
 	empresa.append(Maquina('testtest',5,'basic1','01/01/2023','artur'))
 
 	while True:
-		lang  = input('Digite o numero da operacao desejada:\n' +
+		operacao  = input('Digite o numero da operacao desejada:\n' +
 					  '1. Registrar maquina.\n' +
-					  '2. Verificar maquinas registradas.\n')
-		match lang:
+					  '2. Verificar maquinas registradas.\n' +
+					  '3. Editar maquina registrada.\n')
+		match operacao:
 			case '1':
 				register_machine(empresa)
 				#print(empresa[0].__dict__)
 				continue
 			case '2':
 				check_machines(empresa)
+				continue
+			case '3':
+				edit_machines(empresa)
 				continue
 			case _:
 				break
